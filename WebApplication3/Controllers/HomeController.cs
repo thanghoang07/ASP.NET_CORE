78039cca37adb8de515958bc54660f05a24ae87f
Hoang Duc Thang <thanghoang07@gmail.com>
2017-07-29 10:37:10 +0700

    Add project files.

---
+++ WebApplication3/Controllers/HomeController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //public string Index()
+        //{
+        //    return "Hello thanghoang";
+        //}
+
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Chao buoi sang" : "Chao buoi toi";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpFrom()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpFrom(GuestResponse guestResponse)
+        {
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+    }
+}
